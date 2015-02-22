312359e4cd12dba9966cf94c5cdf0cb84c4a9b36
Kunteper <can158@gmail.com>
2015-02-22 01:02:23 -0500

    UPDATE. INIT DB.

--- web/server/regular_socket.js
+++ web/server/regular_socket.js
@@ -9,10 +9,15 @@ var sql = require('sqlite3');
 var db = new sql.Database(DBNAME);
 
 var dataCount = 0;
-//var newDataEvent = new Event('newData');
 
 var webSock;
 // FUNCTIONS
+function initDatabase(db){
+	db.run("CREATE TABLE if not exists plates(id integer not null primary key autoincrement, street_id integer, timestamp integer, plate varchar(255), lat varchar(255), long varchar(255))");
+	db.run("CREATE TABLE if not exists streets(id integer not null primary key autoincrement, start_lat varchar(255), end_lat varchar(255), start_long varchar(255), end_long varchar(255), max_spots int)");
+	db.run("CREATE TABLE if not exists spots(id integer not null primary key autoincrement, timestamp int, lat varchar(255), long varchar(255), street_id int)");
+}
+
 function getTime(){
 	return Math.floor(Date.now() / 1000);
 }
@@ -34,21 +39,19 @@ function bsGps(pta, ptb, nd){
 }
 
 // WRAPPERS //
-function createTable(db){
-	db.run("CREATE TABLE if not exists dpa (plate varchar(255), time int)");
-}
-
-function dropTable(db){
-	db.run("drop table dpa");
+function dropTables(db){
+	db.run("drop table plates");
+	db.run("drop table streets");
+	db.run("drop table spots");
 }
 //
-
-createTable(db);
+dropTables(db);
+initDatabase(db);
 server.listen(SOCKPORT);
 server.on('connection', function(socket){ // Client connected // 
 	console.log("Client Connected");
 	socket.on('data', function(data){
-		var message = data.toString();
+		var message = data.toString().replace("\r\n", "");
 		message = message.split(" ");
 		console.log(message);
 		switch(message[0]){
@@ -58,7 +61,7 @@ server.on('connection', function(socket){ // Client connected //
 			sttmnt.finalize();
 			console.log("INSERTED");
 			dataCount++;
-			webSock.emit('welcome', { message: message});
+			webSock.emit('welcome', { message: JSON.stringify(message)});
 			break;
 		case "read":
 			db.each("select * from dpa", function(err, row){
@@ -100,7 +103,8 @@ var sockio = require('socket.io').listen(app);
 
 sockio.sockets.on("connection", function(socket){
 	webSock = socket;
-        console.log("ASD", socket);
+	console.log("WEB CLIENT CONNECTED");
+	
 });
 app.listen(IOPORT);
 
